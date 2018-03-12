a1725ee813553b10317b315148dc758de82d290c
EarthChen <179153809@qq.com>
2018-03-12 21:22:38 +0800

    init project

---
+++ src/main/java/com/earthchen/spring/boot/searchhouse/SearchHouseApplication.java
@@ -0,0 +1,17 @@
+package com.earthchen.spring.boot.searchhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+public class SearchHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchHouseApplication.class, args);
+    }
+}
